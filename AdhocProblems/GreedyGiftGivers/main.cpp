c857a4a8bf8b145ffc65b1872055a59040ea18dd
Sai <sai96@hotmail.com>
2014-10-14 11:48:36 +1100

    First iteration of my implementation of algorithm.txt

---
+++ AdhocProblems/GreedyGiftGivers/main.cpp
@@ -0,0 +1,25 @@
+/*
+ID: sai961
+LANG: C++
+PROG: gift1
+*/
+
+#include <iostream>
+#include <fstream>
+
+const std::string inputFile("gift1.in");
+const std::string outputFile("gift1.out");
+
+/*
+Solution to the Greedy Gift Givers problem. (An adhoc problem, i.e. it doesn't fit the mold of any generic algorithms e.g. greedy algo)
+My algorithm design is specified in algorithm.txt.
+*/
+int main(int argc, char* argv[]){
+    //Open the input file for reading.
+    std::ifstream ifs;
+    ifs.open(inputFile.c_str(), std::ifstream::in);
+
+
+
+    return 0;
+}
