413cbb6677db44a3a65d3833647e8ad6a85dc35c
Sai <sai96@hotmail.com>
2014-10-20 12:40:23 +1100

    main.cpp: Incremental update, in the process of implementing the algo

--- CompleteSearch(BruteForce)Problems/MilkingCows/main.cpp
+++ CompleteSearch(BruteForce)Problems/MilkingCows/main.cpp
@@ -9,24 +9,26 @@ PROG: milk2
 #include "milkSchedule.h"
 
 //Creates a schedule from the input.
-milkSchedule createShed();
+void createShed(milkSchedule& newShed);
 
 // The input and output file names.
 const std::string inputFile("milk2.in");
 const std::string outputFile("milk2.out");
 
 int main(int argc, char* argv[]){
-    milkSchedule sched = createShed();
-
+    milkSchedule sched;
+    createShed(sched);
+    //iterate through all the times.
+    for(int i = 0; i < sched.getNumTimes(); i++){
+        std::pair<int,int> nextTime = sched.getNextTime();
+    }
     return 0;
 }
 
 //Reads in the input file and makes a milking schedule object.
-milkSchedule createShed(){
+void createShed(milkSchedule& newSched){
     std::ifstream ifs;
     ifs.open(inputFile.c_str(), std::ifstream::in);
-
-    milkSchedule newSched;
 
     // Ensures that the input file is adequate.
     if(ifs.good()){
@@ -44,6 +46,4 @@ milkSchedule createShed(){
     } else {
         std::cerr << " Error reading input file." << std::endl;
     }
-
-    return newSched;
 }
